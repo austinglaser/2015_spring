b00dba7d2dc30c67c91c5aa34f644948c81273b4
Austin Glaser <austin.glaser@gmail.com>
2015-01-25 03:18:21 -0700

    add tests for submission

---
+++ compilers/hw1/test/submit_tests/cruel_tmp.py
@@ -0,0 +1,14 @@
+tmp1 = input()
+_tmp1 = -tmp1
+tmp_1 = tmp1 + _tmp1
+__tmp1 = input() + - _tmp1 + _tmp1 + tmp1
+_tmp_1 = input() + - __tmp1
+___tmp1 = -__tmp1
+__tmp_1 = _tmp_1 + tmp_1 + -___tmp1
+____tmp1 = __tmp_1 + -_tmp1
+print tmp1
+print _tmp1
+print __tmp1
+print ___tmp1
+print ____tmp1
+print tmp1 + _tmp1 + __tmp1 + ___tmp1 + ____tmp1
---
+++ compilers/hw1/test/submit_tests/nested_addition.py
@@ -0,0 +1,7 @@
+x = 1
+y = 2
+z = x + y
+a = x + y + z
+b = x + y + z + a
+c = x + y + z + a + b
+print c
