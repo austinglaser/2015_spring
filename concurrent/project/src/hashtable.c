2acac4ad73def2adfe5b9dc39414daace2b512f8
Austin Glaser <austin@boulderes.com>
2015-04-21 18:45:03 -0600

    add basic structures and node manipulation functions

--- concurrent/project/src/hashtable.c
+++ concurrent/project/src/hashtable.c
@@ -22,10 +22,44 @@
 
 /* --- PRIVATE DATA TYPES --------------------------------------------------- */
 
-typedef struct hashtable_t_ {
-} hashtable_s;
+/**
+ * @brief   Node storing a single element in the hashtable
+ */
+typedef struct hashtable_node_t_ {
+    hashtable_elem_t            elem;           /**< The stored element */
+    size_t                      hash;           /**< The hash of the key associated with this element */
+    struct hashtable_node_t_ *  next;           /**< The next element in the list (reverse hash ordering) */
+} * hashtable_node_t
 
-/* --- PUBLIC FUNCTION DEFINITIONS------------------------------------------- */
+/**
+ * @brief   The basic data structure for a hash table
+ */
+struct hashtable_t_ {
+    size_t                      n_elements;     /**< The total number of elements stored in the table */
+    size_t                      hash_size;      /**< The number of bits in the hash actually used for binning */
+    hashtable_node_t *          hash_list;      /**< An array of hash bins, of length 2^<hash_size> */
+};
+
+/* --- PRIVATE FUNCTION PROTOTYPES ------------------------------------------ */
+
+/**
+ * @brief   Allocates and returns a new hashtable node structure
+ *
+ * @param[in] elem:     The element for the structure
+ * @param[in] hash:     The associated key hash
+ *
+ * @return:     An allocated hashtable node, or NULL if memory allocation failed
+ */
+static hashtable_node_t hashtable_node_create(hashtable_elem_t elem, size_t hash);
+
+/**
+ * @brief   De-allocates memory associated with a hashtable node
+ *
+ I @param[in] node:     The node to be freed
+ */
+static void hashtable_node_free(hashtable_node_t node);
+
+/* --- PUBLIC FUNCTION DEFINITIONS ------------------------------------------ */
 
 hashtable_t hashtable_create(hash_f_t hash_f)
 {
@@ -54,6 +88,29 @@ hashtable_elem_t hashtable_remove(hashtable_t h, hashtable_key_t key)
 bool hashtable_insert(hashtable_t h, hashtable_key_t key, hashtable_elem_t val)
 {
     return false;
+}
+
+/* --- PRIVATE FUNCTION DEFINITIONS ----------------------------------------- */
+
+static hashtable_node_t hashtable_node_create(hashtable_elem_t elem, size_t hash)
+{
+    // Allocate memory
+    hashtable_node_t node = (hashtable_node_t) malloc(sizeof(struct hashtable_node_t_));
+    if (!node) return NULL;
+
+    // Initialize fields
+    node->elem = elem;
+    node->hash = hash;
+    node->next = NULL;
+
+    // Success
+    return node;
+}
+
+static void hashtable_node_free(hashtable_node_t node)
+{
+    // Deallocate memory
+    if (node) free(node);
 }
 
 /** @} addtogroup HASHTABLE */
